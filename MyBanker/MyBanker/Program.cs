9d975e665bfb9369e7ca70904fda69a93c4ce9eb
Martin Madsen <martin_riehn_madsen@hotmail.com>
2020-11-02 12:06:32 +0100

    Created MyBanker project

---
+++ MyBanker/MyBanker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyBanker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
