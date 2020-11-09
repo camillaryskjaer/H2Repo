acd82358af90d35f3e24698aa1245f7afdfa0541
Martin Madsen <martin_riehn_madsen@hotmail.com>
2020-11-09 08:14:25 +0100

    created project

---
+++ Kaffemaskinen/Kaffemaskinen/Kaffemaskinen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kaffemaskinen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
