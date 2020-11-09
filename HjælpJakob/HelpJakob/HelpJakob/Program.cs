b59f57a6b9eeef98b1bbdb55edd7afb2b0474b8b
Martin Madsen <martin_riehn_madsen@hotmail.com>
2020-11-09 08:46:38 +0100

    created project

---
+++ HjælpJakob/HelpJakob/HelpJakob/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelpJakob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
