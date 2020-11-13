93dcca5f2170496f4d09fbd149ad19334518e1c9
Martin Madsen <martin_riehn_madsen@hotmail.com>
2020-11-13 10:54:03 +0100

    made logging service

---
+++ TestWindowsService/TestWindowsService/Library.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWindowsService
+{
+    public static class Library
+    {
+        static StreamWriter stream = null;
+        public static void WriteErrorLog(Exception ex)
+        {
+            try
+            {
+                stream = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                stream.WriteLine(DateTime.Now.ToString() + ": " + ex.ToString().Trim() + "; " + ex.Message.ToString().Trim());
+                stream.Flush();
+                stream.Close();
+            }
+            catch
+            {
+            }
+        }
+
+        public static void WriteErrorLog(string Message)
+        {
+            try
+            {
+                stream = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                stream.WriteLine(DateTime.Now.ToString() + ": " + Message);
+                stream.Flush();
+                stream.Close();
+            }
+            catch
+            {
+            }
+        }
+    }
+}
--- TestWindowsService/TestWindowsService/Service1.cs
+++ TestWindowsService/TestWindowsService/Service1.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace TestWindowsService
 {
     public partial class Service1 : ServiceBase
     {
+        private Timer timer = null;
         public Service1()
         {
             InitializeComponent();
@@ -19,10 +21,22 @@ namespace TestWindowsService
 
         protected override void OnStart(string[] args)
         {
+            timer = new Timer();
+            timer.Interval = 60000;
+            timer.Elapsed += Timer_Elapsed;
+            timer.Enabled = true;
+            Library.WriteErrorLog("Test windows service started");
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Library.WriteErrorLog("Timer ticked and som job has been done successfully");
         }
 
         protected override void OnStop()
         {
+            timer.Enabled = false;
+            Library.WriteErrorLog("Test windows service stopped");
         }
     }
 }
