20137694844ea19238fc8cb86d8f7b7b86996083
Martin Madsen <martin_riehn_madsen@hotmail.com>
2020-12-14 09:10:40 +0100

    Thread programming started

---
+++ ThreadingConsole/ThreadingConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadingConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Thread tEasy = new Thread(Easy);
+            //tEasy.Start();
+            //Thread tMultiple = new Thread(Multiple);
+            //tMultiple.Start();
+            //tEasy.Join();
+            //tMultiple.Join();
+            Console.ReadLine();
+        }
+
+        //static void Easy()
+        //{
+        //    for (int i = 0; i < 5; i++)
+        //    {
+        //        Console.WriteLine("C#-trådning er nemt!");
+        //        Thread.Sleep(500);
+        //    }
+        //}
+
+        //static void Multiple()
+        //{
+        //    for (int i = 0; i < 5; i++)
+        //    {
+        //        Console.WriteLine("Også med flere tråde...");
+        //        Thread.Sleep(500);
+        //    }
+        //}
+    }
+}
