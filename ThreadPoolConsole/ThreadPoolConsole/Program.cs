0339fe7f16327349cd978cb3c447e44e6c681aba
Martin Madsen <martin_riehn_madsen@hotmail.com>
2020-12-14 11:40:08 +0100

    ø3

--- ThreadPoolConsole/ThreadPoolConsole/Program.cs
+++ ThreadPoolConsole/ThreadPoolConsole/Program.cs
@@ -14,60 +14,80 @@ namespace ThreadPoolConsole
             //    //ThreadPool.QueueUserWorkItem(new WaitCallback(Task2));
             //    ThreadPool.QueueUserWorkItem(Task1);
             //    ThreadPool.QueueUserWorkItem(Task2);
-            //// Forskellen er at vew awaitcallback bliver tråden afviklet færdig inden den næste tråd tager over.
-            //// Uden awaitcallback kørrer trådene som det passer dem.
+            //// Forskellen er at mew awaitcallback bliver tråden afviklet færdig inden den næste tråd tager over.
+            //// Uden waitcallback kørrer trådene som det passer dem.
             //}
 
-            Stopwatch stopwatch = new Stopwatch();
+            //Stopwatch stopwatch = new Stopwatch();
 
-            Console.WriteLine("Threadpool execution");
-            stopwatch.Start();
-            ProcessWithThreadPool();
-            stopwatch.Stop();
-            Console.WriteLine("Time consumed by processwiththreadpool is : " + stopwatch.ElapsedMilliseconds + " milliseconds");
-            stopwatch.Reset();
+            //Console.WriteLine("Threadpool execution");
+            //stopwatch.Start();
+            //ProcessWithThreadPool();
+            //stopwatch.Stop();
+            //Console.WriteLine("Time consumed by processwiththreadpool is : " + stopwatch.ElapsedMilliseconds + " milliseconds");
+            //stopwatch.Reset();
 
-            Console.WriteLine("Thread execution");
-            stopwatch.Start();
-            ProcessWithThreadMethod();
-            stopwatch.Stop();
-            Console.WriteLine("Time consumed by processwiththread is : " + stopwatch.ElapsedMilliseconds + " milliseconds");
-            // ø1
-            // Process methoden skal have object med, da threadpool skal bruge en waitcallback for at kunne køre
-            // Det virker til at den waitcallback godt kan være tom
-            // ø2
-            // Tiden det tager bliver meget længere for almendelig tråd, hvor threadpool ændres tiden ikke ret meget.
+            //Console.WriteLine("Thread execution");
+            //stopwatch.Start();
+            //ProcessWithThreadMethod();
+            //stopwatch.Stop();
+            //Console.WriteLine("Time consumed by processwiththread is : " + stopwatch.ElapsedMilliseconds + " milliseconds");
+            //// ø1
+            //// Process methoden skal have object med, da threadpool skal bruge en waitcallback for at kunne køre
+            //// Det virker til at den waitcallback godt kan være tom
+            //// ø2
+            //// Tiden det tager bliver meget længere for almendelig tråd, hvor threadpool ændres tiden ikke ret meget.
+
+            for (int i = 0; i < 5; i++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(WriteProp));
+            }
+            // ø3
+            // start() - starter tråden
+            // sleep() - pauser tråden i x milisekunder
+            // suspend() - pauser tråden på ubestemt tid
+            // resume() - fortsætter tråden fra sin ubestemte pause
+            // abort() - afslutter tråden, også selv om den ikke er kørt færdig
+            // join() - stopper tråden der opretter denne tråd, indtil denne tråd er kørt færdig
+            // e.x. main fortsætter ikke før tråden er færdig
 
             Console.ReadLine();
         }
 
-        static void ProcessWithThreadPool()
+        static void WriteProp(object callback)
         {
-            for (int i = 0; i < 10; i++)
-            {
-                ThreadPool.QueueUserWorkItem(Process);
-            }
+            Console.WriteLine("Is thread alive : " + Thread.CurrentThread.IsAlive);
+            Console.WriteLine("Is thread background : " + Thread.CurrentThread.IsBackground);
+            Console.WriteLine("Thread priority" + Thread.CurrentThread.Priority);
         }
 
-        static void ProcessWithThreadMethod()
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                Thread obj = new Thread(Process);
-                obj.Start();
-            }
-        }
+        //static void ProcessWithThreadPool()
+        //{
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        ThreadPool.QueueUserWorkItem(Process);
+        //    }
+        //}
 
-        static void Process(object callback)
-        {
-            for (int i = 0; i < 100000; i++)
-            {
-                for (int k = 0; k < 100000; k++)
-                {
+        //static void ProcessWithThreadMethod()
+        //{
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        Thread obj = new Thread(Process);
+        //        obj.Start();
+        //    }
+        //}
 
-                }
-            }
-        }
+        //static void Process(object callback)
+        //{
+        //    for (int i = 0; i < 100000; i++)
+        //    {
+        //        for (int k = 0; k < 100000; k++)
+        //        {
+
+        //        }
+        //    }
+        //}
 
         //static void Task1(object obj)
         //{
