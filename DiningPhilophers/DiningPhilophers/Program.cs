2da6fe68b393487cd6531dc772ce192b4f071349
Martin Madsen <martin_riehn_madsen@hotmail.com>
2020-12-15 09:22:04 +0100

    started assignment

---
+++ DiningPhilophers/DiningPhilophers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace DiningPhilophers
+{
+    class Program
+    {
+        static bool[] forks = new bool[5] { false, false, false, false, false };
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Thread t = new Thread(() => Philosopher(i));
+                t.Start();
+            }
+            while (true)
+            {
+
+            }
+        }
+
+        static void Philosopher(int number)
+        {
+            Random random = new Random();
+
+            while (true)
+            {
+
+            }
+        }
+    }
+}
--- ThreadSynchronizing/ThreadSynchronizing/Program.cs
+++ ThreadSynchronizing/ThreadSynchronizing/Program.cs
@@ -9,29 +9,61 @@ namespace ThreadSynchronizing
         static object _lock = new object();
         static void Main(string[] args)
         {
+            //while (true)
+            //{
+            //    Thread t = new Thread(CountUp);
+            //    t.Start();
+            //    t = new Thread(CountDown);
+            //    t.Start();
+            //    Console.WriteLine(counter);
+            //    Thread.Sleep(1000);
+            //}
+
             while (true)
             {
-                Thread t = new Thread(CountUp);
-                t.Start();
-                t = new Thread(CountDown);
-                t.Start();
-                Console.WriteLine(counter);
-                Thread.Sleep(1000);
+                ThreadPool.QueueUserWorkItem(Stars);
+                ThreadPool.QueueUserWorkItem(Hashtags);
             }
         }
 
-        static void CountUp(object callback)
+        static void Stars(object callback)
         {
             Monitor.Enter(_lock);
-            counter += 2;
+            for (int i = 0; i < 60; i++)
+            {
+                counter++;
+                Console.Write('*');
+            }
+            Console.Write(" " + counter);
+            Console.WriteLine();
             Monitor.Exit(_lock);
         }
 
-        static void CountDown(object callback)
+        static void Hashtags(object callback)
         {
             Monitor.Enter(_lock);
-            counter--;
+            for (int i = 0; i < 60; i++)
+            {
+                counter++;
+                Console.Write('#');
+            }
+            Console.Write(" " + counter);
+            Console.WriteLine();
             Monitor.Exit(_lock);
         }
+
+        //static void CountUp(object callback)
+        //{
+        //    Monitor.Enter(_lock);
+        //    counter += 2;
+        //    Monitor.Exit(_lock);
+        //}
+
+        //static void CountDown(object callback)
+        //{
+        //    Monitor.Enter(_lock);
+        //    counter--;
+        //    Monitor.Exit(_lock);
+        //}
     }
 }
