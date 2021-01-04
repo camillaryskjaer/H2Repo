aa217c78adbb250369a61c92b507d1f36f6a4f96
Martin Madsen <martin_riehn_madsen@hotmail.com>
2021-01-04 09:07:09 +0100

    project started

---
+++ Coincidences/Coincidences/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Coincidences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
