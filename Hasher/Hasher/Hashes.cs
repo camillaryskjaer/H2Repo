c58c9dae085699ff3e65297354506f883afaa5a1
Martin Madsen <martin_riehn_madsen@hotmail.com>
2021-01-04 13:01:11 +0100

    working hashes

---
+++ Hasher/Hasher/Hashes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Hasher
+{
+    public class Hashes
+    {
+        public byte[] Md5Hash(byte[] data)
+        {
+            MD5 mD5 = MD5.Create();
+            return mD5.ComputeHash(data);
+        }
+
+        public byte[] Sha1Hash(byte[] data)
+        {
+            SHA1 sHA1 = SHA1.Create();
+            return sHA1.ComputeHash(data);
+        }
+
+        public byte[] Sha2Hash(byte[] data)
+        {
+            SHA256 sHA256 = SHA256.Create();
+            return sHA256.ComputeHash(data);
+        }
+
+        public byte[] Sha3Hash(byte[] data)
+        {
+            SHA384 sHA384 = SHA384.Create();
+            return sHA384.ComputeHash(data);
+        }
+
+        public byte[] Sha5Hash(byte[] data)
+        {
+            SHA512 sHA512 = SHA512.Create();
+            return sHA512.ComputeHash(data);
+        }
+
+        public byte[] Hmacsha1(byte[] data, byte[] key)
+        {
+            HMACSHA1 hMACSHA1 = new HMACSHA1();
+            hMACSHA1.Key = key;
+            return hMACSHA1.ComputeHash(data);
+        }
+
+        public byte[] HmacSha2(byte[] data, byte[] key)
+        {
+            HMACSHA256 hMACSHA256 = new HMACSHA256();
+            hMACSHA256.Key = key;
+            return hMACSHA256.ComputeHash(data);
+        }
+    }
+}
--- Hasher/Hasher/Program.cs
+++ Hasher/Hasher/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Unicode;
 
 namespace Hasher
 {
@@ -6,7 +10,92 @@ namespace Hasher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Hashes hasher = new Hashes();
+            Stopwatch stopwatch = new Stopwatch();
+            string key;
+
+            Console.WriteLine("Input text to hash");
+            string input = Console.ReadLine();
+
+            Console.WriteLine("Select hash type:");
+            Console.WriteLine("1:SHA1\n2:HMACSHA1\n3:SHA2" +
+                "\n4:HMACSHA2\n5:SHA3\n6:SHA5\n7:MD5");
+            switch (int.Parse(Console.ReadLine()))
+            {
+                case 1:
+                    Console.Clear();
+                    stopwatch.Start();
+                    byte[] hashed = hasher.Sha1Hash(Encoding.ASCII.GetBytes(input));
+                    stopwatch.Stop();
+                    Console.WriteLine("Plain text: " + input);
+                    Console.WriteLine("Hashed: " + Encoding.ASCII.GetString(hashed));
+                    Console.WriteLine("Milliseconds taken: " + stopwatch.ElapsedMilliseconds);
+                    break;
+                case 2:
+                    Console.Clear();
+                    Console.WriteLine("Input key");
+                    key = Console.ReadLine();
+                    stopwatch.Start();
+                    byte[] hmacSha1 = hasher.Hmacsha1(Encoding.ASCII.GetBytes(input), Encoding.ASCII.GetBytes(key));
+                    byte[] hmacSha1Key = Encoding.ASCII.GetBytes(key);
+                    stopwatch.Stop();
+                    Console.WriteLine("Plain text: " + input);
+                    Console.WriteLine("Hashed: " + Encoding.ASCII.GetString(hmacSha1));
+                    Console.WriteLine("Key: " + Encoding.ASCII.GetString(hmacSha1Key));
+                    Console.WriteLine("Milliseconds taken: " + stopwatch.ElapsedMilliseconds);
+                    break;
+                case 3:
+                    Console.Clear();
+                    stopwatch.Start();
+                    byte[] sha2 = hasher.Sha2Hash(Encoding.ASCII.GetBytes(input));
+                    stopwatch.Stop();
+                    Console.WriteLine("Plain text: " + input);
+                    Console.WriteLine("Hashed: " + Encoding.ASCII.GetString(sha2));
+                    Console.WriteLine("Milliseconds taken: " + stopwatch.ElapsedMilliseconds);
+                    break;
+                case 4:
+                    Console.Clear();
+                    Console.WriteLine("Input key");
+                    key = Console.ReadLine();
+                    stopwatch.Start();
+                    byte[] hmacsha2 = hasher.HmacSha2(Encoding.ASCII.GetBytes(input), Encoding.ASCII.GetBytes(key));
+                    byte[] hmac2key = Encoding.ASCII.GetBytes(key);
+                    stopwatch.Stop();
+                    Console.WriteLine("Plain text: " + input);
+                    Console.WriteLine("Hashed: " + Encoding.ASCII.GetString(hmacsha2));
+                    Console.WriteLine("Key: " + Encoding.ASCII.GetString(hmac2key));
+                    Console.WriteLine("Milliseconds taken: " + stopwatch.ElapsedMilliseconds);
+                    break;
+                case 5:
+                    Console.Clear();
+                    stopwatch.Start();
+                    byte[] sha3 = hasher.Sha3Hash(Encoding.ASCII.GetBytes(input));
+                    stopwatch.Stop();
+                    Console.WriteLine("Plain text: " + input);
+                    Console.WriteLine("Hashed: " + Encoding.ASCII.GetString(sha3));
+                    Console.WriteLine("Milliseconds taken: " + stopwatch.ElapsedMilliseconds);
+                    break;
+                case 6:
+                    Console.Clear();
+                    stopwatch.Start();
+                    byte[] sha5 = hasher.Sha5Hash(Encoding.ASCII.GetBytes(input));
+                    stopwatch.Stop();
+                    Console.WriteLine("Plain text: " + input);
+                    Console.WriteLine("Hashed: " + Encoding.ASCII.GetString(sha5));
+                    Console.WriteLine("Milliseconds taken: " + stopwatch.ElapsedMilliseconds);
+                    break;
+                case 7:
+                    Console.Clear();
+                    stopwatch.Start();
+                    byte[] md5 = hasher.Md5Hash(Encoding.ASCII.GetBytes(input));
+                    stopwatch.Stop();
+                    Console.WriteLine("Plain text: " + input);
+                    Console.WriteLine("Hashed: " + Encoding.ASCII.GetString(md5));
+                    Console.WriteLine("Milliseconds taken: " + stopwatch.ElapsedMilliseconds);
+                    break;
+            }
+
+            Console.ReadLine();
         }
     }
 }
