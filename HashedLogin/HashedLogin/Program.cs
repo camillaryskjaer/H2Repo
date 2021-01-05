ba80982b35c4b72f7d6fca6dd13bdfe813a96cf5
Martin Madsen <martin_riehn_madsen@hotmail.com>
2021-01-05 08:50:40 +0100

    project started

---
+++ HashedLogin/HashedLogin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HashedLogin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
